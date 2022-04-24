db2a1046dfc294b1d5a3475fac4135eabe0fe614
YurySamanenko <samanenkoyura@gmail.com>
2022-04-24 15:29:58 +0300

    blog

--- blog/views.py
+++ blog/views.py
@@ -67,7 +67,7 @@ def register_request(request):
             user = form.save()
             login(request, user)
             messages.success(request, "Registration successful.")
-            return redirect("main:homepage")
+            return redirect("login")
         messages.error(request, "Unsuccessful registration. Invalid information.")
     form = NewUserForm()
     return render(request=request, template_name="register.html", context={"form": form})
@@ -83,7 +83,7 @@ def login_request(request):
             if user is not None:
                 login(request, user)
                 messages.info(request, f"You are now logged in as {username}.")
-                return redirect("main:homepage")
+                return redirect("base")
             else:
                 messages.error(request, "Invalid username or password.")
         else:
